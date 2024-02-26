224ca9a1cb08598ca775349ebc1b9a230f24db98
Yahya Andreas Lapian <drelap@hotmail.com>
2024-02-26 13:15:31 +0000

    udate all changes

--- 04-analytics-engineering/taxi_rides_ny/models/core/fact_fhv_trips.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fact_fhv_trips.sql
@@ -12,17 +12,17 @@ dim_zones as (
 select 
     fhv_data.dispatching_base_num, 
     fhv_data.pickup_datetime,
-    fhv_data.dropOff_datetime, 
-    fhv_data.PUlocationID, 
+    fhv_data.dropoff_datetime, 
+    fhv_data.pickup_locationid, 
     pickup_zone.borough as pickup_borough, 
     pickup_zone.zone as pickup_zone, 
-    fhv_data.DOlocationID,
+    fhv_data.dropoff_locationid,
     dropoff_zone.borough as dropoff_borough, 
     dropoff_zone.zone as dropoff_zone,  
-    fhv_data.SR_Flag, 
-    fhv_data.Affiliated_base_number,
+    fhv_data.sr_flag, 
+    fhv_data.affiliated_base_number,
 from fhv_data
 inner join dim_zones as pickup_zone
-on fhv_data.PUlocationID = pickup_zone.locationid
+on fhv_data.pickup_locationid = pickup_zone.locationid
 inner join dim_zones as dropoff_zone
-on fhv_data.DOlocationID = dropoff_zone.locationid
+on fhv_data.dropoff_locationid = dropoff_zone.locationid
